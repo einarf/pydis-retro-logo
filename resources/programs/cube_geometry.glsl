680a404b2e773c4162266d239f1d3cc7ebdae555
Einar Forselv <eforselv@gmail.com>
2019-07-31 06:07:24 +0200

    Derp: Do not apply rotation twice

--- resources/programs/cube_geometry.glsl
+++ resources/programs/cube_geometry.glsl
@@ -5,13 +5,10 @@
 in vec3 in_position;
 in vec3 in_color;
 
-uniform mat4 m_proj;
-uniform mat4 m_model;
-
 out vec3 color;
 
 void main() {
-    gl_Position = m_proj * m_model * vec4(in_position, 1);
+    gl_Position = vec4(in_position, 1);
     color = in_color;
 }
 
