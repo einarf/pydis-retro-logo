0d60a41e05f26294b9cff8000b64d76481b8661d
Einar Forselv <eforselv@gmail.com>
2019-07-31 05:36:17 +0200

    Simplify geometry shader

--- resources/programs/cube_geometry.glsl
+++ resources/programs/cube_geometry.glsl
@@ -38,15 +38,15 @@ vec3 cube_corners[8] = vec3[]  (
 	vec3( 1.0, -1.0,  1.0)  // right bottom near
 );
 
-#define EMIT_V(POS, UV, NORMAL) \
+#define EMIT_V(POS) \
 	gl_Position = m_proj * vec4(POS, 1.0); \
 	EmitVertex()
 
-#define EMIT_QUAD(P1, P2, P3, P4, NORMAL) \
-	EMIT_V(corners[P1], vec2(0.0, 0.0), NORMAL); \
-	EMIT_V(corners[P2], vec2(1.0, 0.0), NORMAL); \
-	EMIT_V(corners[P3], vec2(0.0, 1.0), NORMAL); \
-	EMIT_V(corners[P4], vec2(1.0, 1.0), NORMAL); \
+#define EMIT_QUAD(P1, P2, P3, P4) \
+	EMIT_V(corners[P1]); \
+	EMIT_V(corners[P2]); \
+	EMIT_V(corners[P3]); \
+	EMIT_V(corners[P4]); \
 	EndPrimitive()
 
 void main()
@@ -61,12 +61,12 @@ void main()
 		vec3 pos = point.xyz + cube_corners[i] * 2.0 / 48;
 		corners[i] = (m_model * vec4(pos, 1.0)).xyz;
 	}
-	EMIT_QUAD(3, 2, 0, 1, vec3( 0.0,  0.0, -1.0)); // back
-	EMIT_QUAD(6, 7, 5, 4, vec3( 0.0,  0.0,  1.0)); // front
-	EMIT_QUAD(7, 3, 4, 0, vec3( 1.0,  0.0,  0.0)); // right
-	EMIT_QUAD(2, 6, 1, 5, vec3(-1.0,  0.0,  0.0)); // left
-	EMIT_QUAD(5, 4, 1, 0, vec3( 0.0,  1.0,  0.0)); // top
-	EMIT_QUAD(2, 3, 6, 7, vec3( 0.0, -1.0,  0.0)); // bottom
+	EMIT_QUAD(3, 2, 0, 1); // back
+	EMIT_QUAD(6, 7, 5, 4); // front
+	EMIT_QUAD(7, 3, 4, 0); // right
+	EMIT_QUAD(2, 6, 1, 5); // left
+	EMIT_QUAD(5, 4, 1, 0); // top
+	EMIT_QUAD(2, 3, 6, 7); // bottom
     EndPrimitive();
 }
 
